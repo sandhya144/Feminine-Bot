f92320a5c640bf0a627973aba3a8b166975982aa
sandhya144 <186sndy@gmail.com>
2026-06-26 19:10:40 +0530

    adding backend url

--- frontend/src/components/chatbot/MessageParser.js
+++ frontend/src/components/chatbot/MessageParser.js
@@ -11,7 +11,7 @@ class MessageParser {
     this.msgQ += customMsg;
 
     try {
-      const response = await fetch("http://localhost:5000/api/completions", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/completions`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
